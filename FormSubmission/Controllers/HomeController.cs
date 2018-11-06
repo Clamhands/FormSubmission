52917c3405800bc5bd04bc516fbc0597651e16c0
Clamhands <CooperClopton@gmail.com>
2018-11-06 14:44:58 -0600

    first ccommit

---
+++ FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("Create")]
+        public IActionResult Create(User user)
+        {
+            if(ModelState.IsValid){
+                return RedirectToAction("Success");
+            }
+            else
+            {
+            return View("Index", user);
+
+            }
+
+        }
+
+        public IActionResult Success()
+        {
+
+            return View("Contact");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
